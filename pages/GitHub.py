8828be8bda5e9c0d2d6e2bc6c06be75095d78e2b
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-01-28 15:31:58 -0500

    Update GitHub.py

--- pages/GitHub.py
+++ pages/GitHub.py
@@ -10,16 +10,8 @@ from app import app
 
 # 1 column layout
 column1 = dbc.Col(
-    [
-        dcc.Markdown(
-            """
-        
-            ## GitHub
-            Get more details from our repo at http://https://github.com/KEGANCP/Crime_Predictions_In_Chicago
-            """
-        ),
-
-    ],
-)
+<div class='tableauPlaceholder' id='viz1643401824625' style='position: relative'><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='chicago_crime_story_2&#47;ChicagoCrimeDashboard' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1643401824625');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='1420px';vizElement.style.maxWidth='1620px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='1420px';vizElement.style.maxWidth='1620px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1377px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
 
 layout = dbc.Row([column1])
+
+

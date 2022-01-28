588ff7b83cbc305a6b7054667bd1cde29ec1cdf7
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-01-28 16:50:49 -0500

    Update GitHub.py

--- pages/GitHub.py
+++ pages/GitHub.py
@@ -8,10 +8,25 @@ from dash.dependencies import Input, Output
 # Imports from this application
 from app import app
 
-# 1 column layout
-column1 = dbc.Col(
-<div class='tableauPlaceholder' id='viz1643401824625' style='position: relative'><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='chicago_crime_story_2&#47;ChicagoCrimeDashboard' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1643401824625');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.minWidth='1420px';vizElement.style.maxWidth='1620px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.minWidth='1420px';vizElement.style.maxWidth='1620px';vizElement.style.width='100%';vizElement.style.minHeight='587px';vizElement.style.maxHeight='887px';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1377px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+app.layout = html.Div([
+    html.Div(id='target'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[
+            {'label': 'Video 1', 'value': 'video1'},
+            {'label': 'Video 2', 'value': 'video2'},
+            {'label': 'Video 3', 'value': 'video3'},
+        ],
+        value='video1'
+    )
+])
 
-layout = dbc.Row([column1])
 
-
+@app.callback(Output('target', 'children'), [Input('dropdown', 'value')])
+def embed_iframe(value):
+    videos = {
+        'video1': 'sea2K4AuPOk',
+        'video2': '5BAthiN0htc',
+        'video3': 'e4ti2fCpXMI',
+    }
+    return html.Iframe(src=f'https://www.youtube.com/embed/{videos[value]}')

8fc5e6821a95cbf9bfa2a9be0507cfe20162c9d4
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-01 17:39:57 -0500

    Update tableau.py

--- pages/tableau.py
+++ pages/tableau.py
@@ -4,13 +4,13 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-# import base64
+import base64
 
 # Imports from this application
 from app import app
 
-# image_filename = 'tab.png' 
-# tab = base64.b64encode(open(image_filename, 'rb').read())
+image_filename = 'tab.png' 
+tab = base64.b64encode(open(image_filename, 'rb').read())
 
 # 1 column layout
 column1 = dbc.Col(
@@ -42,29 +42,14 @@ column2 = dbc.Container(
     )
 )
 
-column3 = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            html.P(
-                [
-                    html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
-                    html.Iframe(src="https://public.tableau.com/app/profile/austen.marden/viz/chicago_crime_story_2/ChicagoCrimeDashboard?publish=yes",
-                                style={"height": "1067px", "width": "100%"})
-                ], 
-                className='lead'
-               
-            )
-        )
-    )
-)
-
 # column3 = dbc.Container(
 #     dbc.Row(
 #         dbc.Col(
 #             html.P(
 #                 [
 #                     html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
-#                     html.Img((src='data:image/png;base64,{}'.format(tab))
+#                     html.Iframe(src="https://public.tableau.com/app/profile/austen.marden/viz/chicago_crime_story_2/ChicagoCrimeDashboard?publish=yes",
+#                                 style={"height": "1067px", "width": "100%"})
 #                 ], 
 #                 className='lead'
                
@@ -73,4 +58,22 @@ column3 = dbc.Container(
 #     )
 # )
 
-layout = dbc.Row([column1, column2, column3])
+column3 = dbc.Container(
+    dbc.Row(
+        dbc.Col(
+            html.P(
+                [
+                    html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
+                    def b64_image(image_filename):
+                         with open(image_filename, 'rb') as f:
+                            image = f.read()
+                        return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8'))
+                ], 
+                className='lead'
+               
+            )
+        )
+    )
+)
+
+layout = dbc.Row([column1, column2, column3]) html.Img(src=b64_image(image_filename))

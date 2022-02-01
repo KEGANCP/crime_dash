984bd95164912852a0289e914ebe147c7745d7cd
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-01 18:49:37 -0500

    Update tableau.py

--- pages/tableau.py
+++ pages/tableau.py
@@ -4,43 +4,51 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-# import base64
+import base64
+
 
 # Imports from this application
 from app import app
 
-# image_filename = 'tab.png' 
-# tab = base64.b64encode(open(image_filename, 'rb').read())
+
+image_filename = 'tab.png'
+
+def b64_image(image_filename):
+    with open(image_filename, 'rb') as f:
+        image = f.read()
+    return 'data:image/png;base64,' + base64.b64encode(image).decode('utf-8')
+
+app.layout = html.Img(src=b64_image(image_filename))
 
 # 1 column layout
-column1 = dbc.Col(
-    [
-        dcc.Markdown(
-            """
+# column1 = dbc.Col(
+#     [
+#         dcc.Markdown(
+#             """
         
-            ## Tableau
-            View the below link to view our interactive dashboard via Tableau. These visualizations will provide a good source of our analysis between the correlation between weather and crime in chicago. 
-            """
-        ),
+#             ## Tableau
+#             View the below link to view our interactive dashboard via Tableau. These visualizations will provide a good source of our analysis between the correlation between weather and crime in chicago. 
+#             """
+#         ),
 
-    ],
-)
+#     ],
+# )
 
-column2 = dbc.Container(
-    dbc.Row(
-        dbc.Col(
-            html.P(
-                [
-                    html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
-                    html.A(html.Button('VIEW!', className='three columns'),
-                        href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
-                ], 
-                className='lead'
+# column2 = dbc.Container(
+#     dbc.Row(
+#         dbc.Col(
+#             html.P(
+#                 [
+#                     html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
+#                     html.A(html.Button('VIEW!', className='three columns'),
+#                         href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
+#                 ], 
+#                 className='lead'
                
-            )
-        )
-    )
-)
+#             )
+#         )
+#     )
+# )
 
 # column3 = dbc.Container(
 #     dbc.Row(
@@ -76,4 +84,4 @@ column2 = dbc.Container(
 #     )
 # )
 
-layout = dbc.Row([column1, column2]) 
+# layout = dbc.Row([column1, column2]) 

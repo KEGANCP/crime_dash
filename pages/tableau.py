c4778dee985ca277278a53407eb0f14f925cf15a
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-01 19:08:59 -0500

    Update tableau.py

--- pages/tableau.py
+++ pages/tableau.py
@@ -1,43 +1,14 @@
+# Import libraries
 import dash
-from dash import html
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 import base64
 
-app = dash.Dash()
-
-image_filename = '/assets/tab.png' # replace with your own image
-encoded_image = base64.b64encode(open(image_filename, 'rb').read())
-
-app.layout = html.Div([
-    html.Img(src='data:image/png;base64,{}'.format(encoded_image))
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Import libraries
-# import dash
-# import dash_bootstrap_components as dbc
-# import dash_core_components as dcc
-# import dash_html_components as html
-# from dash.dependencies import Input, Output
-# import base64
-
-
-# # Imports from this application
-# from app import app
+app.layout = html.Div(html.Img(src=app.get_asset_url('tab.png')))
+# Imports from this application
+from app import app
 
 
 # image_filename = 'tab.png'
@@ -49,35 +20,35 @@ if __name__ == '__main__':
 
 # app.layout = html.Img(src=b64_image(image_filename))
 
-# 1 column layout
-# column1 = dbc.Col(
-#     [
-#         dcc.Markdown(
-#             """
+1 column layout
+column1 = dbc.Col(
+    [
+        dcc.Markdown(
+            """
         
-#             ## Tableau
-#             View the below link to view our interactive dashboard via Tableau. These visualizations will provide a good source of our analysis between the correlation between weather and crime in chicago. 
-#             """
-#         ),
+            ## Tableau
+            View the below link to view our interactive dashboard via Tableau. These visualizations will provide a good source of our analysis between the correlation between weather and crime in chicago. 
+            """
+        ),
 
-#     ],
-# )
+    ],
+)
 
-# column2 = dbc.Container(
-#     dbc.Row(
-#         dbc.Col(
-#             html.P(
-#                 [
-#                     html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
-#                     html.A(html.Button('VIEW!', className='three columns'),
-#                         href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
-#                 ], 
-#                 className='lead'
+column2 = dbc.Container(
+    dbc.Row(
+        dbc.Col(
+            html.P(
+                [
+                    html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
+                    html.A(html.Button('VIEW!', className='three columns'),
+                        href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
+                ], 
+                className='lead'
                
-#             )
-#         )
-#     )
-# )
+            )
+        )
+    )
+)
 
 # column3 = dbc.Container(
 #     dbc.Row(
@@ -113,4 +84,4 @@ if __name__ == '__main__':
 #     )
 # )
 
-# layout = dbc.Row([column1, column2]) 
+layout = dbc.Row([column1, column2]) 

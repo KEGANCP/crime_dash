5f2ae1503f4ddfbe258e14b65465ff03fd92b40a
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-03 18:31:31 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import flask 
-import dash_dangerously_set_inner_html
+# import dash_dangerously_set_inner_html
 
 # Imports from this application
 from app import app, server

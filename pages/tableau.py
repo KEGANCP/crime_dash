34e339b1d35aa94f4b63fd8b2f61f732bc0aa0c9
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-02 21:53:42 -0500

    Update tableau.py

--- pages/tableau.py
+++ pages/tableau.py
@@ -70,7 +70,7 @@ column2 = dbc.Container(
                 [
                     html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
                     html.A(html.Button('VIEW!', className='three columns',
-                           href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link', style={'color':'blue'})
+                           href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
                 ], 
                 className='lead'
                

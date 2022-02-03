04f8747790328e52cd3af28675c8c54056e7bb56
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-02 21:31:30 -0500

    Update run.py

--- run.py
+++ run.py
@@ -46,8 +46,8 @@ footer = dbc.Container(
             html.P(
                 [
                     html.Span('Crime Prediction Team', className='mr-2'), 
-                    html.A("Kegan Propster", href='https://www.linkedin.com/in/kegan-propster', target="_blank")
-                    html.A("Sean Farr", href='https://www.linkedin.com/in/kegan-propster', target="_blank")
+                    html.A("Kegan Propster", href='https://www.linkedin.com/in/kegan-propster', target="_blank"),
+                    html.A("Sean Farr", href='https://www.linkedin.com/in/kegan-propster', target="_blank"),
                     html.A("Austen Marden", href='https://www.linkedin.com/in/kegan-propster', target="_blank")
                 ], 
                 className='lead'

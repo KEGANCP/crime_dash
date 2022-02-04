ee576c972948be913bed87f64b4e43a190cdf3df
Kegan Propster <88690046+KEGANCP@users.noreply.github.com>
2022-02-03 19:19:43 -0500

    Update tableau.py

--- pages/tableau.py
+++ pages/tableau.py
@@ -19,7 +19,7 @@ column1 = dbc.Col(
             """
         
             ## Tableau
-            View our Tableau Dashboard here: https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link
+            View our interactive Tableau Dashboard below. 
             """
         ),
 
@@ -32,8 +32,8 @@ column2 = dbc.Container(
             html.P(
                 [
                     html.Span('Chicago Crime & Weather Visualizations', className='mr-2'), 
-                    html.A(html.Button('VIEW!', className='three columns'),
-                        href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
+#                     html.A(html.Button('VIEW!', className='three columns'),
+#                         href='https://public.tableau.com/views/chicago_crime_story_2/ChicagoCrimeDashboard?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link')
                 ], 
                 className='lead'
                
@@ -54,7 +54,7 @@ column3 = html.Div([
                     'padding-top' : 0,
                     'padding-right' : 0
                 })
-    ], href='https://chicagocrimepredictions.herokuapp.com/tableau')
+    ], href='https://public.tableau.com/app/profile/austen.marden/viz/chicago_crime_story_2/ChicagoCrimeDashboard')
 ]) 
 
 # column3 = dbc.Container(
